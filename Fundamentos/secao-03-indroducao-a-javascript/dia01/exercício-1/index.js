4c17680cd709f5e98cf8ddd94c235103be2b5997
Victor Guilherme <victorguilherme6345@gmail.com>
2023-06-01 10:41:36 -0300

    exercício 1 js

---
+++ Fundamentos/secao-03-indroducao-a-javascript/dia01/exercício-1/index.js
@@ -0,0 +1,9 @@
+let num1 = 2
+let num2 = 2
+
+let soma = num1 + num2
+let subtração = num1 -num2
+let multiplicao = num1 * num2
+let divisão = num1/num2
+
+console.log(soma, subtração, multiplicao, divisão)
