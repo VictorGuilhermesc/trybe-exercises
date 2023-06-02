24a7af840a095a42f83568104268d5d79f6919bb
Victor Guilherme <victorguilherme6345@gmail.com>
2023-06-02 10:55:54 -0300

    exercício completo

--- Fundamentos/secao-03-indroducao-a-javascript/dia01/exercício-7/script.js
+++ Fundamentos/secao-03-indroducao-a-javascript/dia01/exercício-7/script.js
@@ -1,3 +1,5 @@
+//Este programa calcula o salário de uma pessoa descontando o imposto de reda e o INSS
+
 let salarioBruto = prompt('Digite seu salário bruto:')
 
 let salarioLiquido
@@ -11,7 +13,7 @@ if (salarioBruto <= 1556.94){
     descontoInss = (salarioBruto * 8) / 100
     salarioLiquido = salarioBruto - descontoInss
 
-} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92){
     descontoInss = (salarioBruto * 9) / 100
     salarioLiquido = salarioBruto - descontoInss
 
@@ -26,7 +28,20 @@ if (salarioBruto <= 1556.94){
 }
 //caucula o IR
 
-if(salarioBruto > 2594.93 && salarioBruto < 5189.82){
+if (salarioLiquido <= 1903.98) {
+    console.log('Você está isento do imposto de renda')
 
-    descontoIr = 2594.93
+} else if (salarioLiquido >= 1903.99 && salarioLiquido <= 2826.65){
+    salarioLiquido = salarioLiquido - [(salarioLiquido * 7.5) / 100]
+
+} else if (salarioLiquido >= 2826.66 && salarioLiquido <= 3751.05){
+    salarioLiquido = salarioLiquido - [(salarioLiquido * 7.5) / 100]
+
+} else if (salarioLiquido >= 3751.05 && salarioLiquido <= 4664.68){
+    salarioLiquido = salarioLiquido - [(salarioLiquido * 7.5) / 100]
+
+} else if(salarioLiquido > 4664.68){
+    salarioLiquido = salarioLiquido - [(salarioLiquido * 7.5) / 100]
 }
+
+alert(`O seu salário Liquido é de ${salarioLiquido}`)
