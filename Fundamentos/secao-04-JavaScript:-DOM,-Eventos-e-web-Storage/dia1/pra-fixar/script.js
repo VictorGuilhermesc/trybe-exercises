ad6b2aeaf43bcd277ed79f77167ac6478f7546e4
Victor Guilherme <victorguilherme6345@gmail.com>
2023-06-17 15:54:53 -0300

    dia1

---
+++ Fundamentos/secao-04-JavaScript:-DOM,-Eventos-e-web-Storage/dia1/pra-fixar/script.js
@@ -0,0 +1,18 @@
+document.getElementsByTagName('title')[0].innerText = "The Batman"
+
+document.getElementsByTagName('h1')[0].innerText = 'The Batman'
+
+document.getElementsByTagName('h2')[0].innerText = 'Pricipais informações'
+
+document.getElementsByTagName('p')[0].innerHTML = 'he Batman (Brasil: Batman / Portugal: The Batman) é um filme de super-herói estadunidense, baseado no personagem Batman da editora DC Comics, um reboot da franquia Batman produzido pela DC Films, 6th & Idaho e Dylan Clark Productions, e dirigido por Matt Reeves, roteirista junto com Peter Craig. É estrelado por Robert Pattinson como Bruce Wayne / Batman, ao lado de Zoë Kravitz, Paul Dano, e Colin Farrell. Que segue a história de Batman em seu segundo ano de luta contra o crime em busca de vingança, quando descobre a corrupção em Gotham City enquanto persegue o Charada (Dano), um assassino em série que tem como alvo a elite de Gotham.[9]'
+
+let paragrafo = document.getElementsByClassName("paragrafo")[0]
+
+paragrafo.style.fontFamily = "Arial"
+
+
+document.getElementsByTagName('p')[1].innerText= 'direção: nome da pessoa que dirigiu o filme Roteirista: Nome da(s) pesssoa(s) responsável(is) pelo roteiro'
+
+let h2 = document.getElementsByTagName("h2")[0]
+
+h2.style.color = 'red'
