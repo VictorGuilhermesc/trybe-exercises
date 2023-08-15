038d8663ebb08d715bb692d964e80bf76bbdc2ec
Victor Guilherme <victorguilherme6345@gmail.com>
2023-08-15 16:59:51 -0300

    exercicio feito

---
+++ Front-end/secao-02-introducao-ao-react/dia1/exercicio/src/components/HelloWorld.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Title from './Title';
+import ModuleDetails from './ModuleDetails';
+
+function HelloWorld() {
+  return (
+    <div>
+      <Title />
+      <ModuleDetails />
+    </div>
+  );
+}
+
+export default HelloWorld;
---
+++ Front-end/secao-02-introducao-ao-react/dia1/exercicio/src/components/ModuleDetails.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ModuleDetails() {
+  return (
+    <ul>
+      <li>Fundamentos</li>
+      <li>Front-end</li>
+      <li>backend</li>
+      <li>sof-skills</li>
+    </ul>
+  );
+}
+
+export default ModuleDetails;
