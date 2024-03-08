1bc86730fdd65a74753d0afc1e2924eb99a95fe0
Victor Guilherme <victorguilherme6345@gmail.com>
2024-03-08 04:08:24 -0300

    att

---
+++ Back-end/seção:introdução-ao-desenvolvimento-web-com-node.js/dia2/soccer-team-manager/src/app.js
@@ -0,0 +1,60 @@
+// src/app.js
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+  res.status(201).json({ team: newTeam });
+});
+
+app.get('/teams', (req, res) => res.status(201).json({ teams }));
+
+// altera o time através do id
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+
+  const updateTeam = teams.find((team) => team.id === Number(id));
+
+  if (!updateTeam) {
+    return res.status(404).json({ message: 'Team not found' });
+  }
+
+  updateTeam.name = name;
+  updateTeam.initials = initials;
+  res.status(200).json({ updateTeam });
+});
+
+// pega os times pelo id 
+app.get('/teams/:id', (req, res) => {
+  const team = teams.find(({ id }) => id === Number(req.params.id));
+  res.status(200).json(team);
+});
+
+// deleta o time pelo id
+app.delete('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const arrayPosition = teams.findIndex((team) => team.id === Number(id));
+  teams.splice(arrayPosition, 1);
+
+  res.status(200).end();
+});
+
+module.exports = app;
