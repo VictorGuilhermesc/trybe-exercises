8184d0d817085ec9292b94bed54c73dfe88802f0
Victor Guilherme <victorguilherme6345@gmail.com>
2024-03-12 11:30:58 -0300

    att

---
+++ Back-end/seção:introdução-ao-desenvolvimento-web-com-node.js/dia4/soccer-team-manager/src/middlewares/existingId.js
@@ -0,0 +1,15 @@
+// src/middlewares/existingId.js
+const teams = require('../utils/teams');
+
+const existingId = (req, res, next) => {
+  const id = Number(req.params.id);
+  if (teams.some((t) => t.id === id)) {
+    // se existe, a requisição segue para o próximo middleware
+    next();
+  } else {
+    // se não existe, então vamos retornar o status HTTP 404
+    res.sendStatus(404);
+  }
+};
+
+module.exports = existingId;
