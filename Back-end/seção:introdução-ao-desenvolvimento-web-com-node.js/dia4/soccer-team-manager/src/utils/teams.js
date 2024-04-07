d6024adb72a09d633930cf90fc8a9f796017734a
Victor Guilherme <victorguilherme6345@gmail.com>
2024-04-06 23:07:00 -0300

    att

--- Back-end/seção:introdução-ao-desenvolvimento-web-com-node.js/dia4/soccer-team-manager/src/utils/teams.js
+++
@@ -1,8 +0,0 @@
-// src/utils/teams.js
-
-const teams = [
-  { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
-  { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
-];
-
-module.exports = teams;
