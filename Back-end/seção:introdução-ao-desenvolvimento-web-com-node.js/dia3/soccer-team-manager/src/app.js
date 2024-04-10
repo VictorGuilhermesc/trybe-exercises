f91cfd2aff6a468f4000542e14c93467d43e32f6
Victor Guilherme <victorguilherme6345@gmail.com>
2024-04-10 18:49:19 -0300

    adiconando middlewares

--- Back-end/seção:introdução-ao-desenvolvimento-web-com-node.js/dia3/soccer-team-manager/src/app.js
+++ Back-end/seção:introdução-ao-desenvolvimento-web-com-node.js/dia3/soccer-team-manager/src/app.js
@@ -4,33 +4,13 @@ const app = express();
 
 app.use(express.json());
 
-const teams = [
-  {
-    id: 1,
-    name: 'São Paulo Futebol Clube',
-    initials: 'SPF',
-  },
-  {
-    id: 2,
-    name: 'Clube Atlético Mineiro',
-    initials: 'CAM',
-  },
-];
+const validateTeam = require('./middlewares/validateTeam');
+const existingId = require('./middlewares/existingID');
+const teams = require('./utils/teams');
 
 // app.get('/', (req, res) => res.status(200).json({ message: 'Olá, mundo!'}))
 
 // app.get('/', (req, res) => res.status(200).json({ teams }))
-
-const existingId = (req, res, next) => {
-  const id = Number(req.params.id);
-  
-  if (teams.some((t) => t.id === id)) {
-    // se existe, a requisição segue para o próximo middleware
-    return next();
-  }
-  // se não existe, então vamos retornar o status HTTP 404
-  res.sendStatus(404);
-};
 
 // adiciona um novo time
 app.post('/teams', (req, res) => {
@@ -62,7 +42,7 @@ app.get('/teams/:id', (req, res) => {
   res.status(200).json(team);
 });
 
-app.put('/teams/:id', existingId, (req, res) => {
+app.put('/teams/:id', existingId, validateTeam, (req, res) => {
   const id = Number(req.params.id);
   const team = teams.find((t) => t.id === id);
   // não precisamos mais conferir, com certeza o team existe
